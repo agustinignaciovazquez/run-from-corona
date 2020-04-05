484cb2b03d1c5b5edcb426c9856fe3cbb8a33cb8
Agustín Vazquez <agvazquez@itba.edu.ar>
2020-04-04 21:28:10 -0300

    pool objects

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToPool;
+    [SerializeField] private int amountToPool;
+    
+    public static ObjectPooler SharedInstance;
+    private List<GameObject> pooledObjects;
+    
+    void Awake() {
+        SharedInstance = this;
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pooledObjects = new List<GameObject>();
+        for (int i = 0; i < amountToPool; i++) {
+            GameObject obj = (GameObject)Instantiate(objectToPool);
+            obj.SetActive(false); 
+            pooledObjects.Add(obj);
+        }
+    }
+    
+    public GameObject GetPooledObject() {
+        for (int i = 0; i < pooledObjects.Count; i++) {
+            if (!pooledObjects[i].activeInHierarchy) {
+                return pooledObjects[i];
+            }
+        }
+        return null;
+    }
+}
--- Assets/Scripts/ScrollingBackground.cs
+++ Assets/Scripts/ScrollingBackground.cs
@@ -8,17 +8,36 @@ public class ScrollingBackground : MonoBehaviour
     private Rigidbody2D rb;
     private float scrollVelocity;
     
+    private float backgroundSize;
+    private float backgroundLimitOffset;
+    
     // Start is called before the first frame update
     void Awake()
     {
+        backgroundSize = GetComponent<SpriteRenderer>().size.x; 
+        backgroundLimitOffset = backgroundSize;
         rb = GetComponent<Rigidbody2D>();
         scrollVelocity = player.GetComponent<PlayerController>().getScrollingSpeed();
         rb.velocity = new Vector2(-scrollVelocity, 0f);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         //TODO ADD Difficulty throw time
+        Transform backgroundTransform = transform;
+        Vector3 backGroundPosition = backgroundTransform.position;
+        if (backGroundPosition.x  + backgroundLimitOffset < 0)
+        {
+            RepositionBackground(backgroundTransform);
+        }
+    }
+    
+    private void RepositionBackground(Transform backgroundTransform)
+    {
+        Vector2 groundOffset = new Vector2(backgroundSize * 2.0f,0);
+        Vector2 newBackgroundPosition = (Vector2) backgroundTransform.position + groundOffset;
+        backgroundTransform.position = newBackgroundPosition;
     }
 }
