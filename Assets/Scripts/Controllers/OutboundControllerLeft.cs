b511bfe653051045c53411659011084ecd71f5a4
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2020-05-23 20:18:23 -0300

    Working on UI

---
+++ Assets/Scripts/Controllers/OutboundControllerLeft.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutboundControllerLeft : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        //Outbounds
+        //Delete other object
+        print("OUTBOUNDS");
+        other.gameObject.SetActive(false);
+    }
+    
+}
+
---
+++ Assets/Scripts/Controllers/OutboundControllerRight.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutboundControllerRight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    protected virtual void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            //Outbounds
+            //Delete other object
+            print("OUTBOUNDS");
+            other.gameObject.SetActive(false);
+        }
+        
+    }
+    
+}
