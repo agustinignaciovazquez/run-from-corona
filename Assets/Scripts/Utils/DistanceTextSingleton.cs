1cabd7ad7165e85dec43f873b3256891bc0108b0
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2020-05-31 20:34:29 -0300

    Adding Singletons UI

---
+++ Assets/Scripts/Utils/DistanceTextSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DistanceTextSingleton : MonoBehaviour
+{
+    [SerializeField] private Text distanceText;
+    public static DistanceTextSingleton SharedInstance { get; private set; }
+    private void Awake(){
+
+        if (SharedInstance == null){
+
+            SharedInstance = this;
+            DontDestroyOnLoad(this.gameObject);
+            
+        } else {
+            Destroy(this);
+        }
+    }
+    
+    public void SetDistance(int amount)
+    {
+        distanceText.text = "x" + amount;
+    }
+
+}
