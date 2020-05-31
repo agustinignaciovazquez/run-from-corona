08a5f20a943903fab835021564919425d41f5a4b
Santiago Manganaro Bello <smanganaro@itba.edu.ar>
2020-05-31 20:43:15 -0300

    Working Distance

--- Assets/Scripts/Utils/DistanceTextSingleton.cs
+++ Assets/Scripts/Utils/DistanceTextSingleton.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class DistanceTextSingleton : MonoBehaviour
 {
     [SerializeField] private Text distanceText;
+    [SerializeField] private GameObject distanceReference;
     public static DistanceTextSingleton SharedInstance { get; private set; }
     private void Awake(){
 
@@ -18,10 +19,15 @@ public class DistanceTextSingleton : MonoBehaviour
             Destroy(this);
         }
     }
-    
+
+    public void Update()
+    {
+        float distance = distanceReference.transform.position.x * -1f;
+        SetDistance((int)distance);
+    }
     public void SetDistance(int amount)
     {
-        distanceText.text = "x" + amount;
+        distanceText.text = amount + "m";
     }
 
 }
