3f9d2a6e2eba65a6eab60035ee243c947762b9ba
Agustín Vazquez <agvazquez@itba.edu.ar>
2020-06-18 16:24:13 -0300

    cambios q me faltaban pushear

---
+++ Assets/LoadMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LoadMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+    }
+    
+}
